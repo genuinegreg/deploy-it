083d44455b9958c87b14b977df6f49ae00b1cff2
Grégoire Audoux <projects@gregoire-audoux.fr>
2013-02-11 16:51:29 +0100

    Add scafolded dployApi service

---
+++ app/scripts/services/dployApi.js
@@ -0,0 +1,15 @@
+'use strict';
+
+dployApp.factory('dployApi', ['$http', function($http) {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+        getApps: function() {
+
+        }
+    };
+}]);
---
+++ test/spec/services/dployApi.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Service: dployApi', function () {
+
+  // load the service's module
+  beforeEach(module('dployApp'));
+
+  // instantiate service
+  var dployApi;
+  beforeEach(inject(function(_dployApi_) {
+    dployApi = _dployApi_;
+  }));
+
+  it('should do something', function () {
+    expect(!!dployApi).toBe(true);
+  });
+
+});
