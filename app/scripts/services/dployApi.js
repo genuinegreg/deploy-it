ad95beb4ca0fe44fdf6c6e3bc45b2c206950211d
Grégoire Audoux <projects@gregoire-audoux.fr>
2013-02-22 13:13:12 +0100

    Update jsonApi list mock data and list view

--- app/scripts/services/dployApi.js
+++ app/scripts/services/dployApi.js
@@ -10,11 +10,28 @@ dployApp.factory('dployApi', ['$http', function($http) {
         getApps: function() {
 
             return [
-                '123456',
-                '654321',
-                '789456',
-                '897654'
-            ]
+                {
+                    name: "MyApp",
+                    upload: new Date(2013, 1, 12, 15, 32,0),
+                    version: 5,
+                    hash: "654321",
+                    download: 6
+                },
+                {
+                    name: "OtherApp",
+                    upload: new Date(2013, 0, 29, 14, 10),
+                    version: 1,
+                    hash: "123456",
+                    download: 15
+                },
+                {
+                    name: "SomeApp",
+                    upload: new Date(2013, 1, 19, 9, 59),
+                    version: 16,
+                    hash: "abc54d",
+                    download: 46
+                }
+            ];
 
             // $http.post(conf.http.host + 'getApp', {})
         }
