d9f54c97631a47f8c75ba1520884c10d8b77a371
Grégoire Audoux <projects@gregoire-audoux.fr>
2013-02-27 15:28:56 +0100

    Run test on port 9005 to avoid error if grunt server is runing

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,7 +73,7 @@ module.exports = function (grunt) {
             },
             test: {
                 options: {
-                    port: 9000,
+                    port: 9005,
                     middleware: function (connect) {
                         return [
                             mountFolder(connect, '.tmp'),
