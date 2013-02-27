832e7fac923edf8262ba5b7d32e0e7e22053178b
Grégoire Audoux <projects@gregoire-audoux.fr>
2013-02-27 13:16:02 +0100

    Update Provisioner script

--- Provisioner.sh
+++ Provisioner.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+apt-get update -y
+
+apt-get install -y software-properties-common
+
 apt-add-repository -y ppa:chris-lea/node.js
 apt-get update -y
 
 apt-get install -y nodejs npm phantomjs build-essential git unzip libplist-utils
 
 
-npm install -g yo grunt-cli bower generator-angular generator-testacular
+npm install -g yo grunt-cli bower
