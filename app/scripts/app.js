7861f0bd4227e467591e4816ba659d7523b4954c
Grégoire Audoux <projects@gregoire-audoux.fr>
2013-03-07 11:12:31 +0100

    Update bootstrap plugins

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,21 +2,21 @@
 
 angular.module('dployApp', [])
     .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'views/main.html',
-            controller: 'MainCtrl'
-        })
-        .when('/list', {
-            templateUrl: 'views/list.html',
-            controller: 'ListCtrl'
-        })
-        .when('/:hash', {
-            templateUrl: 'views/install.html',
-            controller: 'InstallCtrl'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-}]);
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/list', {
+                templateUrl: 'views/list.html',
+                controller: 'ListCtrl'
+            })
+            .when('/:hash', {
+                templateUrl: 'views/install.html',
+                controller: 'InstallCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
 
